5238660651e87f038dea27b050d89864e9716d91
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-01-18 02:17:09 -0600

    Prepare files and pom.xml to be deployed. (#1)
    * Prepare files/config for deployment/Travis.

--- src/test/java/io/opentracing/akka/TracedAbstractActorTest.java
+++ src/test/java/io/opentracing/akka/TracedAbstractActorTest.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2017-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.opentracing.akka;
 
 import akka.actor.ActorRef;
@@ -11,7 +24,9 @@ import io.opentracing.mock.MockTracer;
 import io.opentracing.util.GlobalTracer;
 import io.opentracing.util.ThreadLocalScopeManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
@@ -30,6 +45,8 @@ public class TracedAbstractActorTest {
 
     @Before
     public void before() throws Exception {
+        TestUtils.resetGlobalTracer(); // safe start, just in case.
+
         mockTracer.reset();
         GlobalTracer.register(mockTracer);
 
@@ -38,8 +55,9 @@ public class TracedAbstractActorTest {
 
     @After
     public void after() throws Exception {
-        TestUtils.resetGlobalTracer();
-        system.terminate(); // TODO - wait for actual termination?
+        TestUtils.resetGlobalTracer(); // clean up.
+
+        Await.result(system.terminate(), TestUtils.getDefaultDuration());
     }
 
     static abstract class TestActor extends TracedAbstractActor {
